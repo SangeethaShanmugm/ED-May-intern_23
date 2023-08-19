74370b4dc896b62b7cea6eb254ea5b7bc8fd0311
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-08-19 19:14:13 +0530

    updated

--- node/restaurantApi/apidocument.md
+++ node/restaurantApi/apidocument.md
@@ -11,3 +11,56 @@ http://localhost:5000/quickSearch
 List of restaurants
 
 http://localhost:5000/restaurants
+
+restaurant wrt city
+http://localhost:5000/restaurants?state_id=1
+
+## Page 2
+
+restaurant wrt meal type
+http://localhost:5000/restaurants?mealId=6
+
+restaurant wrt cuisine & meal
+
+http://localhost:5000/filter/1?cuisineId=2
+
+restaurant wrt cost & meal
+
+http://localhost:5000/filter/1?lcost=800&hcost=1000
+
+sort on basis of cost
+
+http://localhost:5000/filter/1?lcost=300&hcost=500&sort=-1
+
+## Page 3
+
+Details of a restaurant
+
+http://localhost:5000/details/1
+
+menu of restaurant
+
+http://localhost:5000/menu/2
+
+## Page 4
+
+Menu Details (POST)
+http://localhost:5000/menuItem
+
+PlaceOrder (POST)
+
+## Page 5
+
+List of orders
+
+List of orders wrt to email
+
+Update payment details (PUT)
+
+delete orders (DELETE)
+
+CRUD
+C - Create - POST
+R - Read - GET
+U - Update - PUT
+D - Delete - DELETE
--- node/restaurantApi/index.js
+++ node/restaurantApi/index.js
@@ -154,14 +154,105 @@ app.get("/quickSearch", function (req, res) {
     });
 });
 
-//get restaurant data
+//get restaurant data by stateId || mealId
 app.get("/restaurants", function (req, res) {
+  let query = {};
+  let stateId = Number(req.query.state_id);
+  let mealId = Number(req.query.mealId);
+  if (stateId) {
+    query = { state_id: stateId };
+  } else if (mealId) {
+    query = { "mealTypes.mealtype_id": mealId };
+  }
+
   db.collection("restaurant")
-    .find()
+    .find(query)
     .toArray((err, result) => {
       if (err) throw err;
       res.send(result);
     });
+});
+
+//filter
+
+app.get("/filter/:mealId", function (req, res) {
+  let query = {};
+  let mealId = Number(req.params.mealId);
+  let cuisineId = Number(req.query.cuisineId);
+  let lcost = Number(req.query.lcost);
+  let hcost = Number(req.query.hcost);
+  let sort = { cost: 1 };
+  if (req.query.sort) {
+    sort = { cost: req.query.sort };
+  }
+
+  if (cuisineId) {
+    query = {
+      "mealTypes.mealtype_id": mealId,
+      "cuisines.cuisine_id": cuisineId,
+    };
+  } else if (lcost && hcost) {
+    query = {
+      "mealTypes.mealtype_id": mealId,
+      $and: [{ cost: { $gt: lcost, $lt: hcost } }],
+    };
+  } else if (cuisineId && lcost && hcost) {
+    query = {
+      "mealTypes.mealtype_id": mealId,
+      "cuisines.cuisine_id": cuisineId,
+      $and: [{ cost: { $gt: lcost, $lt: hcost } }],
+    };
+  }
+  //-1 => desc
+  //1 => asc
+  db.collection("restaurant")
+    .find(query)
+    .sort(sort)
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+//details of a restaurant
+
+app.get("/details/:id", function (req, res) {
+  let id = Number(req.params.id);
+
+  db.collection("restaurant")
+    .find({ restaurant_id: id })
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+//menu of restaurant
+
+app.get("/menu/:id", function (req, res) {
+  let id = Number(req.params.id);
+
+  db.collection("menu")
+    .find({ restaurant_id: id })
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+//Menu Details
+
+app.post("/menuItem", express.json(), function (req, res) {
+  if (Array.isArray(req.body)) {
+    db.collection("menu")
+      .find({ menu_id: { $in: req.body } })
+      .toArray((err, result) => {
+        if (err) throw err;
+        res.send(result);
+      });
+  } else {
+    res.send("Invalid Input");
+  }
 });
 
 //Mongodb connection
